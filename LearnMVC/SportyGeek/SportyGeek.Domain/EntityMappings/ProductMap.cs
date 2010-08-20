94735a8993a8ce72f3194530fef4d9f1c6d826a7
Josh Perry <josh@6bit.com>
2010-08-20 13:38:57 -0600

    forgot to add the mapping file

---
+++ LearnMVC/SportyGeek/SportyGeek.Domain/EntityMappings/ProductMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+using SportyGeek.Domain.Entities;
+
+namespace SportyGeek.Domain.EntityMappings
+{
+    class ProductMap : ClassMap<Product>
+    {
+        public ProductMap()
+        {
+            Map(x => x.Name)
+                .Length(50);
+
+            Map(x => x.Description)
+                .Length(500);
+
+            Map(x => x.Category)
+                .Length(50);
+        }
+    }
+}
